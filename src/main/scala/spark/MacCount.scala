c9f863f2abb7b536576bdc8d353a8e663b53c693
zhenyang.li <zhenyang.li@changhong.com>
2020-11-10 11:00:14 +0800

    macCount增加错误条数统计

--- src/main/scala/spark/MacCount.scala
+++ src/main/scala/spark/MacCount.scala
@@ -9,12 +9,20 @@ object MacCount {
 
     val conf = new SparkConf()
     val sc = new SparkContext(conf)
+
     val macRecord = sc.longAccumulator("MacRecord")
+    val emptyMac = sc.longAccumulator("emptyMac")
 
     val input = sc.textFile("hdfs://hadoop1:9000"+args(0))
 
     input.map(record => JSON.parseObject(record))
-      .filter(record => StringUtil.isNotEmpty(record.getString("query_mac")))
+      .filter(record => {
+        if(StringUtil.isEmpty(record.getString("query_mac"))){
+          emptyMac.add(1)
+          return false
+        }
+        true
+      })
       .map(record => record.getString("query_mac"))
       .distinct()
       .foreach(_ => macRecord.add(1))
