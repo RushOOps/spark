b5fb9d81e8fe12b2e4c0afd67ac66795816b5fe6
zhenyang.li <zhenyang.li@changhong.com>
2020-12-02 09:55:03 +0800

    分词词语统计

--- src/main/scala/spark/Word.scala
+++ src/main/scala/spark/Word.scala
@@ -5,40 +5,40 @@ import org.apache.spark.{SparkConf, SparkContext}
 import util.StringUtil
 
 import scala.collection.mutable.ArrayBuffer
-import scala.io.Source
 
-object Word extends App {
+object Word {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    //  val conf = new SparkConf().setMaster("local[*]").setAppName("test")
+    val sc = new SparkContext(conf)
+    val processCount = sc.longAccumulator("processCount")
 
-  val conf = new SparkConf()
-//  val conf = new SparkConf().setMaster("local[*]").setAppName("test")
-  val sc = new SparkContext(conf)
+    val words = sc.textFile(System.getenv("SPARK_YARN_STAGING_DIR")+args(1)).collect().toList
+    val broadcast = sc.broadcast(words)
 
-  val wordFile = Source.fromFile("hdfs://hadoop1:9000"+args(1))
-//  val wordFile = Source.fromFile(args(1))
-  val broadcast = sc.broadcast(wordFile.getLines().toList)
-  wordFile.close
-
-  val input = sc.textFile("hdfs://hadoop1:9000"+args(0))
-//  val input = sc.textFile(args(0))
-  input
-    .map(JSON.parseObject)
-    .filter(record => StringUtil.isNotEmpty(record.getString("query_text")))
-    .map(record => record.getString("query_text"))
-    .distinct
-    .flatMap(record => {
-      val result = new ArrayBuffer[(String, Int)]()
-      for(s <- broadcast.value){
-        if(record.contains(s.split(" ")(0))){
-          result.append((s, 1))
+    val input = sc.textFile(args(0))
+    //  val input = sc.textFile(args(0))
+    input
+      .map(JSON.parseObject)
+      .filter(record => StringUtil.isNotEmpty(record.getString("query_text")))
+      .map(record => record.getString("query_text"))
+      .distinct
+      .flatMap(record => {
+        processCount.add(1)
+        val result = new ArrayBuffer[(String, Int)]()
+        for(s <- broadcast.value){
+          if(record.contains(s.split(" ")(0))){
+            result.append((s, 1))
+          }
         }
-      }
-      result
-    })
-    .reduceByKey(_+_)
-    .map(record => record._1+" "+record._2)
-    .repartition(1)
-    .saveAsTextFile("hdfs://hadoop1:9000"+args(2))
-//    .saveAsTextFile(args(2))
+        result
+      })
+      .reduceByKey(_+_)
+      .map(record => record._1+" "+record._2)
+      .repartition(1)
+      .saveAsTextFile(args(2))
 
-  sc.stop()
+    sc.stop()
+  }
+
 }
