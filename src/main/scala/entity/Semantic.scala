c2b3d142993c2e73b5f2d3ff4f20a172a6e9ad36
zhenyang.li <zhenyang.li@changhong.com>
2020-12-03 15:51:50 +0800

    SmartHome 新增实体 Semantic 修改构造方法，以对应更多需求

--- src/main/scala/entity/Semantic.scala
+++ src/main/scala/entity/Semantic.scala
@@ -5,8 +5,9 @@ import com.alibaba.fastjson.JSONObject
 class Semantic(
                 val queryText: String,
                 val domain: String,
-                val intent: String,
-                val semantic: JSONObject) extends Serializable {
+                val intent: String = null,
+                val semantic: JSONObject = null,
+                val sourceFlag: Int = -1) extends Serializable {
 
   override def hashCode(): Int = {
     var result = 17
@@ -18,7 +19,7 @@ class Semantic(
   override def equals(obj: Any): Boolean = {
     obj match {
       case other: Semantic =>
-       other.queryText.equals(this.queryText) && other.domain.equals(this.domain)
+        other.queryText.equals(this.queryText) && other.domain.equals(this.domain)
       case _ =>
         false
     }
---
+++ src/main/scala/entity/SmartHome.scala
@@ -0,0 +1,33 @@
+package entity
+
+import com.alibaba.fastjson.JSONObject
+
+class SmartHome(val device: String,
+                val queryText: String,
+                val domain: String,
+                val intent: String,
+                val semantic: JSONObject) extends Serializable {
+
+  override def hashCode(): Int = {
+    var result = 17
+    result = result * 31 + queryText.hashCode
+    result
+  }
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case other: SmartHome =>
+        other.queryText.equals(this.queryText)
+      case _ =>
+        false
+    }
+  }
+
+//  override def toString: String = {
+//    "device: " + device + "\n" +
+//      "queryText: " + queryText + "\n" +
+//      "domain: " + domain + "\n" +
+//      "intent: " + intent + "\n" +
+//      "semantic" + semantic.toString() + "\n"
+//  }
+}
