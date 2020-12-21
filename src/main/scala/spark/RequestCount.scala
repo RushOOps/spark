93d965def1b98a010d9d27f72ccdcea622c285f7
zhenyang.li <zhenyang.li@changhong.com>
2020-12-21 17:46:32 +0800

    统计指定领域的请求量

---
+++ src/main/scala/spark/RequestCount.scala
@@ -0,0 +1,28 @@
+package spark
+
+import com.alibaba.fastjson.JSON
+import org.apache.spark.{SparkConf, SparkContext}
+
+import java.time.LocalDate
+
+object RequestCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf
+    val sc = new SparkContext(conf)
+
+    val input = sc.textFile("hdfs://hadoop1:9000/execDir")
+    val domains = sc.textFile(System.getenv("SPARK_YARN_STAGING_DIR")+"/domains.txt").collect.toList
+
+    val domainsBC = sc.broadcast(domains)
+
+    input
+      .map(JSON.parseObject)
+      .filter(record => domainsBC.value.contains(record.getString("return_domain")))
+      .map(record => (record.getString("return_domain"), 1))
+      .reduceByKey(_+_)
+      .repartition(1)
+      .saveAsTextFile("hdfs://hadoop1:9000/output_"+LocalDate.now)
+
+    sc.stop()
+  }
+}
