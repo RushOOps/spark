e0ec9c2e5133dcb645afab03477c4a5750466f26
zhenyang.li <zhenyang.li@changhong.com>
2021-03-01 13:36:56 +0800

    sparkstreaming updatebykey注释更新

--- src/main/scala/spark/streaming/StreamingUpdateByKey.scala
+++ src/main/scala/spark/streaming/StreamingUpdateByKey.scala
@@ -14,6 +14,7 @@ object StreamingUpdateByKey {
     val input = ssc.socketTextStream("47.93.6.72", 2333)
     val pairs = input.flatMap(_.split(" ")).map((_, 1))
 
+    // 只需要提供新旧值的计算方式，底层源码已实现对每个key的新旧值计算调用该函数，再更新key的state
     pairs.updateStateByKey(updateFunction).print()
 
     ssc.start()
