3cce9bf82ae1cd2168886c8ce959aa0ee7043a8a
zhenyang.li <zhenyang.li@changhong.com>
2021-03-25 10:28:06 +0800

    parser走630mac统计

--- src/main/scala/spark/rdd/Common.scala
+++ src/main/scala/spark/rdd/Common.scala
@@ -3,6 +3,7 @@ package spark.rdd
 import com.alibaba.fastjson.JSON
 import com.mongodb.spark.MongoSpark
 import org.apache.spark.{SparkConf, SparkContext}
+import org.bson.Document
 import util.StringUtil
 
 /**
@@ -13,7 +14,7 @@ object Common {
 
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().set("spark.mongodb.output.uri", "mongodb://semantic:semantic22s2@172.17.1.181:27017/semantic."+args(0))
+    val conf = new SparkConf().set("spark.mongodb.output.uri", "mongodb://semantic:semantic22s2@172.17.1.181:27017/semantic.parser_1228-1231")
     val sc = new SparkContext(conf)
 
     val input = sc.textFile("hdfs://hadoop1:9000/execDir")
@@ -21,15 +22,19 @@ object Common {
     val result = input
       .map(JSON.parseObject)
       .filter(record => {
-        val query = record.getJSONObject("query")
-        query != null && query.getString("isNew").equals("0") && StringUtil.isNotEmpty(record.getString("query_mac"))
+        record.getString("version") != null && record.getString("version").equals("201806")
       })
       .map(record => (record.getString("query_mac"),1))
       .reduceByKey(_+_)
+      .map(record => {
+        new Document()
+          .append("mac", record._1)
+          .append("count", record._2)
+      })
 
     MongoSpark.save(result)
 
-    sc.stop()
+//    sc.stop()
   }
 
 }
